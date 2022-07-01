a68220bf3f7ee6768d826b81f6b626fd91a545ac
alfyandoo <alfyando2001@gmail.com>
2022-07-01 21:02:54 +0700

    fixing crud :construction:

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,41 +14,43 @@ const checkStorage = () => {
 document.addEventListener('DOMContentLoaded', () => {
     const recordBook = document.querySelector('#recordBook');
 
+    recordBook.addEventListener('submit', (e) => {
+        e.preventDefault();
+        addBook();
+
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#year').value = '';
+        document.querySelector('#isComplete').checked = false;
+    });
+
+
+    const editForm = document.querySelector('#editBook');
+    console.log(editBook);
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        editBook();
+
+        const record = document.querySelector('#record');
+        record.removeAttribute('hidden');
+
+        const edit = document.querySelector('#edit');
+        edit.setAttribute('hidden', true);
+    });
+
     if (checkStorage()) {
         loadDataBooksFromStorage();
     }
-
-    recordBook.addEventListener('click', (e) => {
-        e.preventDefault();
-        addBook();
-    });
 });
 
-const saveBook = () => {
-    if (checkStorage()) {
-        const parse = JSON.stringify(books);
-        localStorage.setItem(STORAGE_KEY, parse);
-        document.dispatchEvent(new Event(SAVED_BOOK));
-    }
-}
+const cancelEdit = document.querySelector('#cancelEdit');
+cancelEdit.addEventListener('click', () => {
+    const record = document.querySelector('#record');
+    record.removeAttribute('hidden');
 
-document.addEventListener(SAVED_BOOK, () => {
-    console.log(localStorage.getItem(STORAGE_KEY));
+    const edit = document.querySelector('#edit');
+    edit.setAttribute('hidden', true);
 });
-
-const loadDataBooksFromStorage = () => {
-    const listBook = localStorage.getItem(STORAGE_KEY);
-
-    let dataBooks = JSON.parse(listBook);
-
-    if (dataBooks !== null) {
-        for (list of dataBooks) {
-            books.push(list);
-        }
-    }
-
-    document.dispatchEvent(new Event(RENDER_BOOK));
-}
 
 const addBook = () => {
     const getId = getIdBook();
@@ -66,11 +68,23 @@ const addBook = () => {
     saveDataBooks();
 }
 
-const saveDataBooks = () => {
-    if (checkStorage()) {
-        const parse = JSON.stringify(books);
-        localStorage.setItem(STORAGE_KEY, parse);
-        document.dispatchEvent(new Event(SAVED_BOOK));
+const editBook = () => {
+    const idBook = parseInt(document.querySelector('#idBook').value);
+    console.log(`idBook: ${idBook}`);
+    for (item of books) {
+        console.log(`item.id: ${item.id}`);
+        console.log(`idBook: ${idBook}`);
+        console.log(item.id === idBook);
+        if (item.id === idBook) {
+            item.title = document.querySelector('#editTitle').value;
+            item.author = document.querySelector('#editAuthor').value;
+            item.year = document.querySelector('#editYear').value;
+            item.isComplete = document.querySelector('#editIsComplete').checked;
+
+            document.dispatchEvent(new Event(RENDER_BOOK));
+
+            saveDataBooks();
+        }
     }
 }
 
@@ -88,20 +102,46 @@ const generateBookObject = (id, title, author, year, isComplete) => {
     }
 }
 
-document.addEventListener(RENDER_BOOK, () => {
-    const notFinishedYet = document.querySelector('#notFinishedYetList');
-    notFinishedYet.innerHTML = '';
+const saveDataBooks = () => {
+    if (checkStorage()) {
+        const parse = JSON.stringify(books);
+        localStorage.setItem(STORAGE_KEY, parse);
+        document.dispatchEvent(new Event(SAVED_BOOK));
+    }
+}
 
-    const finished = document.querySelector('#finishedList');
-    finished.innerHTML = '';
+document.addEventListener(SAVED_BOOK, function () {
+    console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+const loadDataBooksFromStorage = () => {
+    const listBook = localStorage.getItem(STORAGE_KEY);
+
+    let dataBooks = JSON.parse(listBook);
+
+    if (dataBooks !== null) {
+        for (list of dataBooks) {
+            books.push(list);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_BOOK));
+}
+
+document.addEventListener(RENDER_BOOK, () => {
+    const notFinishedReading = document.querySelector('#notFinishedReadingList');
+    notFinishedReading.innerHTML = '';
+
+    const finishedReading = document.querySelector('#finishedReadingList');
+    finishedReading.innerHTML = '';
 
     for (item of books) {
         const bookElement = createBookElement(item);
 
         if (item.isComplete) {
-            finished.append(bookElement);
+            finishedReading.append(bookElement);
         } else {
-            notFinishedYet.append(bookElement);
+            notFinishedReading.append(bookElement);
         }
     }
 });
@@ -113,7 +153,7 @@ const createBookElement = (booksObject) => {
     bookTitle.innerHTML = title;
 
     const bookAuthor = document.createElement('p');
-    bookAuthor.innerHTML = author;
+    bookAuthor.innerHTML = `Author: ${author}`;
 
     const bookYear = document.createElement('p');
     bookYear.innerHTML = year;
@@ -124,34 +164,35 @@ const createBookElement = (booksObject) => {
     containerList.setAttribute('id', `id-${id}`);
 
     if (isComplete) {
-        const undoButton = document.createElement('button');
-        undoButton.classList.add('undo');
-        undoButton.innerHTML = 'Not Finished Yet';
-
-        const removeButton = document.createElement('button');
-        removeButton.classList.add('delete');
-        removeButton.innerHTML = 'Delete';
+        const notFinishedButton = document.createElement('button');
+        notFinishedButton.classList.add('not-finished-book');
+        notFinishedButton.innerHTML = 'Not Finished';
 
         const editButton = document.createElement('button');
-        editButton.classList.add('edit');
+        editButton.classList.add('edit-book');
         editButton.innerHTML = 'Edit';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-book');
+        deleteButton.innerHTML = 'Delete';
+
 
         const tagDiv = document.createElement('div');
         tagDiv.classList.add('action');
-        tagDiv.append(undoButton, removeButton, editButton);
+        tagDiv.append(notFinishedButton,  editButton, deleteButton);
 
-        undoButton.addEventListener('click', () => {
-            addToNorFinishedYet(id);
+        notFinishedButton.addEventListener('click', () => {
+            addToNotFinishedReading(id);
         })
 
-        removeButton.addEventListener('click', () => {
-            const modal = document.querySelector('#modal');
+        deleteButton.addEventListener('click', () => {
+            const modal = document.querySelector('.modal');
             modal.style.display = 'block';
 
-            const validate = document.querySelector('.validate');
+            const validate = document.querySelector('.validation');
 
             validate.addEventListener('click', () => {
-                removeBook(id);
+                deleteBook(id);
                 modal.style.display = 'none';
             })
 
@@ -161,19 +202,188 @@ const createBookElement = (booksObject) => {
             })
         });
 
-        editButton.addEventListener('click', () => {
-            const editBook = document.querySelector('#editBook');
-            editBook.style.display = 'block';
-
-            const bookSubmit = document.querySelector('#book_submit');
-
-            bookSubmit.addEventListener('click', (e) => {
-                e.preventDefault();
-                editBookDetail(id);
-                editBook.style.display = 'none';
-            });
+        editButton.addEventListener("click", function () {
+            showFormEdit(id);
         });
 
         containerList.append(tagDiv);
-    } else {}
+    } else {
+        const finishedButton = document.createElement('button');
+        finishedButton.classList.add('finished-book');
+        finishedButton.innerText = 'Finished';
+
+        const editButton = document.createElement('button');
+        editButton.classList.add('edit-book');
+        editButton.innerText = 'Edit';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-book');
+        deleteButton.innerText = 'Delete';
+
+
+        const tagDiv = document.createElement('div');
+        tagDiv.classList.add('action');
+        tagDiv.append(finishedButton, editButton, deleteButton);
+
+        finishedButton.addEventListener('click', () => {
+            addToFinishedReading(id);
+        });
+
+        deleteButton.addEventListener('click', () => {
+            const modalButton = document.querySelector('#modalButton');
+            modalButton.style.display = 'block';
+
+            const validasi = document.querySelector('.validation');
+            validasi.addEventListener('click', () => {
+                deleteBook(id);
+                modalButton.style.display = 'none';
+            });
+
+            const cancelDelete = document.querySelector('.cancel');
+            cancelDelete.addEventListener('click',() => {
+                modalButton.style.display = 'none';
+            });
+        });
+
+        editButton.addEventListener('click', function () {
+            showFormEdit(id);
+        });
+
+        containerList.append(tagDiv);
+    }
+    return containerList;
+}
+
+const showFormEdit = (id) => {
+    for (const item of books) {
+      if (item.id === id) {
+        const addForm = document.querySelector("#record");
+        addForm.setAttribute("hidden", true);
+  
+        const editForm = document.querySelector("#edit");
+        editForm.removeAttribute("hidden");
+  
+        document.querySelector("#idBook").value = item.id;
+        document.querySelector("#editTitle").value = item.title;
+        document.querySelector("#editAuthor").value = item.author;
+        document.querySelector("#editYear").value = item.year;
+        document.querySelector("#editIsComplete").checked = item.isComplete;
+      }
+    }
+    location.href = '#edit';
+  }
+
+const addToFinishedReading = (id) => {
+    const target = findId(id);
+    console.log(target);
+    if (target == null) {
+        return;
+    }
+
+    target.isComplete = true;
+    document.dispatchEvent(new Event(RENDER_BOOK));
+
+    saveDataBooks();
+}
+
+const addToNotFinishedReading = (id)=> {
+        const target = findId(id);
+        if (target == null) {
+            return;
+        }
+    
+        target.isComplete = false;
+        document.dispatchEvent(new Event(RENDER_BOOK));
+    
+        saveDataBooks();
+    }
+
+const findId = (id) => {
+    for (item of books) {
+        if (item.id === id) {
+            return item;
+        }
+    }
+    return null;
+}
+
+const findIndex = (id) => {
+    for (index in books) {
+        if (books[index].id === id) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+const deleteBook = (id) => {
+    const bookTarget = findIndex(id);
+    if (bookTarget === -1) {
+        return;
+    }
+    books.splice(bookTarget, 1);
+
+    document.dispatchEvent(new Event(RENDER_BOOK));
+
+    saveDataBooks();
+}
+
+const editBookDetail = (id) => {
+    const targetEdit = findIndex(id);
+    if (targetEdit === -1) {
+        return;
+    }
+    for (const item of books) {
+        if (item.id === id) {
+            console.log('tes')
+          item.title = document.querySelector("#editTitle").value;
+          item.author = document.querySelector("#editAuthor").value;
+          item.year = document.querySelector("#editYear").value;
+          item.isComplete = document.querySelector("#editIsComplete").checked;
+
+          document.dispatchEvent(new Event(RENDER_EVENT));
+          
+          saveData();
+        }
+      }
+    document.dispatchEvent(new Event(RENDER_BOOK));
+
+    saveDataBooks();
+}
+
+const buttonSearch = document.querySelector('#searchTitle');
+const searchValue = document.querySelector('#searchByTitle');
+
+buttonSearch.addEventListener('click', () => {
+    if (localStorage.getItem(STORAGE_KEY) == null) {    
+        return alert("There Is No Book Data")
+    } else {
+        const getByTitle = JSON.parse(localStorage.getItem(STORAGE_KEY)).filter(a => a.title == searchValue.value.trim());
+        if (getByTitle.length == 0) {
+            alert(`Data Not Found With Keywords: ${searchValue.value}`)
+        } else {
+            showSearchResult(getByTitle);
+        }
+    }
+
+    searchValue.value = ''
+});
+
+const showSearchResult = (books) => {
+    const searchResult = document.querySelector("#searchResult")
+
+    searchResult.innerHTML = ''
+
+    books.forEach(book => {
+        let element = `
+        <h2 class="text-center text-result">Result</h2>
+        <article class="book_list">
+            <h3>${book.title}</h3>
+            <p>Author : ${book.author}</p>
+            <p>${book.year}</p>
+            <p>Status : ${book.isComplete ? 'Finished Reading' : 'Not Finished Reading'}</p>
+        </article>
+        `
+        searchResult.innerHTML += element;
+    });
 }
